4210ebca2fb2488a2f263bb2c7790bfa35618eed
JoubertVictor <johnvict323@gmail.com>
2025-01-10 01:27:51 -0300

    Add NavePrincipla imag

--- src/Naves/Player/NavePlayer.py
+++ src/Naves/Player/NavePlayer.py
@@ -1,12 +1,16 @@
 import pygame
-
-class Player:
-    def __init__(self, x, y):
-        self.image = pygame.image.load("path_to_player_image.png")
-        self.x = x
-        self.y = y
-        self.velocity = 5
-    
+import src.Interface.jogo_space as jogo_space
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        # Carrega a imagem do jogador
+        self.image = pygame.image.load("src/Assents/NavePrincipal.jpg")  # Ajuste a escala conforme necessário
+        self.rect = self.image.get_rect()
+        self.rect.centerx =  self.screen_width// 2
+        self.rect.bottom = self.screen_height - 10
+        self.speed = 5
+        self.cooldown = 250
+        self.last_shot = pygame.time.get_ticks()
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
     
