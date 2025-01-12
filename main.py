594232de6dc03e9d6324b26fa18cfffa9be4dd34
JoubertVictor <johnvict323@gmail.com>
2025-01-12 18:51:27 -0300

    Fazendo Rodar

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from Interface.interface import MainMenu
+import pygame
+from pygame.sprite import Group
+
+from Auxiliar.settings import Settings
+from Auxiliar.game_stats import GameStats
+from Auxiliar.scoreboard import Scoreboard
+from Auxiliar.button import Button
+from Auxiliar.ship import Ship
+import Auxiliar.game_functions as gf
 
 # Instancia e executa o menu principal
 if __name__ == "__main__":
